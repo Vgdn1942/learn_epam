b1b7873e7fa1114b824d6ed3dec2d03def9d1272
Василий Ковалёв <vgdn1942@gmail.com>
2021-02-11 16:53:13 +0300

    Train successfully

--- src/com/vgdn1942/progwithclasses/Train.java
+++ src/com/vgdn1942/progwithclasses/Train.java
@@ -1,83 +1,71 @@
 package com.vgdn1942.progwithclasses;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
 class RunTrain {
     public static void main(String[] args) {
-        int input = 789;
+        Scanner scanner = new Scanner(System.in);
 
         Train[] train = {
-                new Train("Минск", 321, 1405),
-                new Train("Брест", 123, 1310),
-                new Train("Могилёв", 789, 1245),
-                new Train("Гомель", 654, 1515),
-                new Train("Витебск", 456, 2315)
+                new Train("Минск", 321, "14:05"),
+                new Train("Пинск", 432, "14:15"),
+                new Train("Минск", 123, "13:10"),
+                new Train("Брест", 789, "12:45"),
+                new Train("Брест", 654, "15:15"),
+                new Train("Витебск", 456, "23:15")
         };
 
+        System.out.print("Введите номер поезда;" + "\n" + "доступные номера:" + "\n");
+        for (Train value : train) {
+            System.out.print(value.getNumber() + "; ");
+        }
+        System.out.println();
+        int input = scanner.nextInt();
+
         // Информация о поезде по номеру
-        System.out.println("Информация о поезде по введённому номеру " + input + ": ");
+        System.out.println("Информация о поезде под номером " + input + ": ");
         for (Train value : train) {
             if (input == value.getNumber()) {
                 System.out.println(value.toString() + "\n");
             }
         }
 
-        // Сортировка по номеру поезда
-        System.out.println("Список поездов отсортированный по номерам:");
-        int[] array = new int[train.length];
-        for (int i = 0; i < train.length; i++) {
-            array[i] = train[i].getNumber();
-        }
-        String[] strArray = new String[train.length];
-        for (int i = 0; i < train.length; i++) {
-            strArray[i] = train[i].getDestination();
-        }
-        for (int i = 0; i < strArray.length; i++) {
-            for (int j = i + 1; j < strArray.length; j++) {
-                if (strArray[i] == strArray[j]) {
-                    System.out.println(j);
-                }
-            }
-        }
-
-        boolean sorted = false;
-        int temp;
-        while (!sorted) {
-            sorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
-                    sorted = false;
-                }
-            }
-        }
-        for (int k : array) {
-            for (Train value : train) {
-                if (k == value.getNumber()) {
-                    System.out.println(value.toString());
-                }
-            }
+        System.out.println("Сортировка по пункту назначения и времени:");
+        Arrays.sort(train, new CompareToDestAndTime());
+        for (Train value : train) {
+            System.out.println(value.toString());
         }
     }
 }
 
-// 4. Создайте класс Train, содержащий поля:
-// название пункта назначения,
-// номер поезда,
-// время отправления.
-// Создайте данные в массив из пяти элементов типа Train,
-// добавьте возможность сортировки элементов массива по номерам поездов.
-// Добавьте возможность вывода информации о поезде,
-// номер которого введен пользователем.
-// Добавьте возможность сортировки массив по пункту назначения,
-// причем поезда с одинаковыми пунктами назначения должны быть упорядочены по времени отправления.
+class CompareToDestAndTime implements Comparator<Train> {
+    @Override
+    public int compare(Train trainLeft, Train trainRight) {
+        if (trainLeft.getDestination().equals(trainRight.getDestination())) {
+            return trainLeft.getTime().compareTo(trainRight.getTime());
+        }
+        return trainLeft.getDestination().compareTo(trainRight.getDestination());
+    }
+}
 
 public class Train {
     private String destination;
     private int number;
-    private long time;
+    private String time;
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 
     public String getDestination() {
         return destination;
@@ -87,20 +75,20 @@ public class Train {
         return number;
     }
 
-    public long getTime() {
+    public String getTime() {
         return time;
     }
 
-    Train(String destination, int number, long time) {
-        this.destination = destination;
-        this.number = number;
-        this.time = time;
+    Train(String destination, int number, String time) {
+        this.setDestination(destination);
+        this.setNumber(number);
+        this.setTime(time);
     }
 
     @Override
     public String toString() {
         return "Пункт назначения: " + this.getDestination() +
                 "; номер: " + this.getNumber() +
-                "; время отправления (ччмм): " + this.getTime();
+                "; время отправления: " + this.getTime();
     }
 }
